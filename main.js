2dd745b6d5689d0d166ed6ccd95bd52615ab99e1
Narendra Rathod <narurathod55@gmail.com>
2020-10-12 16:28:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var btn = document.getElementById('btn');
+var getUsers = document.getElementById('users');
+btn.addEventListener('click', loadUsers);
+
+function loadUsers() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('Get', 'https://api.github.com/users/narurathod', true);
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var users = JSON.parse(this.responseText);
+            var output = '';
+            // for (var i in users) {
+            output += '<div class="user">' + '<img src="' + users.avatar_url + '" width="70">' + '<ul>' +
+                '<li> Login: ' + users.login + '</li>' +
+                '<li> Id: ' + users.id + '</li>' +
+                '</ul>' + '</div>'
+            getUsers.innerHTML = output;
+            // }
+
+            // console.log(this.responseText);
+        }
+
+    }
+    xhr.send();
+}
