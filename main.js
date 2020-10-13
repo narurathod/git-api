885ebb98763779706af97acb1da3ff0c745036d6
Narendra Rathod <narurathod55@gmail.com>
2020-10-13 08:24:24 +0530

    Update main.js
    added all the users from git hub

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ function loadUsers() {
         if (this.status == 200) {
             var users = JSON.parse(this.responseText);
             var output = '';
-            // for (var i in users) {
-            output += '<div class="user">' + '<img src="' + users.avatar_url + '" width="70">' + '<ul>' +
-                '<li> Login: ' + users.login + '</li>' +
-                '<li> Id: ' + users.id + '</li>' +
+             for (var i in users) {
+            output += '<div class="user">' + '<img src="' + users[i].avatar_url + '" width="70">' + '<ul>' +
+                '<li> Login: ' + users[i].login + '</li>' +
+                '<li> Id: ' + users[i].id + '</li>' +
                 '</ul>' + '</div>'
             getUsers.innerHTML = output;
-            // }
+             }
 
             // console.log(this.responseText);
         }
